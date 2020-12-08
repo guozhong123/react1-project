2897cc76c07d6564765ae5e7e2c7739f4318ab4c
zhongguo <1994838315@qq.com>
2020-12-08 16:52:18 +0800

    第一次提交，react脚手架

---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import Home from '../pages/home'
+import NotFound from '../pages/NotFound'
+export const router = [
+  {
+    path:'/home',
+    component:Home
+  },
+  {
+    path:'/NotFound',
+    component:NotFound
+  }
+]
